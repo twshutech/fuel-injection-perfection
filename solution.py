631cac141a6c56860218567cc4db3802f10a1bef
shuwei <38966382+twshutech@users.noreply.github.com>
2021-01-20 16:03:38 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,45 +1,36 @@
-class node:
-  def __init__(self, n):
-    self.pellets = n
-    self.left = None
-    self.right = None
-
-  def sub_nodes(self):
-    if self.pellets % 2 == 0:
-      self.left = node(self.pellets // 2)
+def recursion(n):
+  while n > 1:
+    global moves
+    if n&1 == 0:  #Remain 0 by divide 2.
+      n >>= 1
     else:
-      self.left = node(self.pellets + 1)
-      self.right = node(self.pellets - 1)
-
-def fuel_control(pellets):
-  current_level = []
-  foot_print = [n for row in pellets for n in row]
-  for n in pellets[-1]:
-    c = node(n)
-    c.sub_nodes()
-
-    if c.left != None and c.left.pellets not in current_level and c.left.pellets not in foot_print:
-      current_level.append(c.left.pellets)
-    if c.right != None and c.right.pellets not in current_level and c.right.pellets not in foot_print:
-      current_level.append(c.right.pellets)
-  pellets.append(current_level)
-  if 1 not in current_level:
-    fuel_control(pellets)
-  else:
-    return len(pellets)-1
+      if n == 3 or n % 4 ==1:
+        n-=1
+      else:
+        n+=1
+    moves+=1
 
 def solution(n):
-  global moves
-  moves = []
-  moves.append([int(n)])
-  fuel_control(moves)
-  return len(moves) -1
+  look_up = [3, 4]
+  foot_print = []
+  import math
+  global moves, look_up_destination, power_times, is_integer
+  is_integer = lambda a: a.is_integer()
+  power_times = lambda a:math.log(a, 2)
+  look_up_destination = lambda a:a in look_up
 
+  n = int(n)
+  moves = 0
+  recursion(n)
+  return moves
+print solution('124480579411363943422977485485450829978158403576349485507396127987323092328068524587695005561434534623452345436346456353425362283769712245781118297614280332424240701048410620648401132628401374562794562558123463462235342526466804149296501029546541499918765438784295157088047123009825235235168758962399')
 
 def ask():
+  print 'Insert random integer as destination from 1.'
   destination = input()
-  if destination == 0:
+  if isinstance(destination, int) == False:
     return
   else:
+    print 'It took',solution(destination),'moves to reach destination', destination
     ask()
 ask()
